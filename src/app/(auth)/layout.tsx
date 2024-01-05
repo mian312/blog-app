1fb51e85aeafd5b704b1a1302943a3bdf8a51495
Milan <deymilan312@gmail.com>
2024-01-05 21:59:03 +0530

    Created authentication service

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import '@/app/globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+    title: 'Authenticate...',
+    description: 'A Social Media App',
+}
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <body className={`${inter.className} flex justify-center`}>
+                {children}
+            </body>
+        </html>
+    )
+}
