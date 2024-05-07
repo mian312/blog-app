6d70a817146c40cd6516e714a7b5cd6d0e8d541e
Milan <deymilan312@gmail.com>
2024-05-07 12:20:14 +0530

    Added update post (caption & tag) page

---
+++ src/app/(root)/edit-post/[id]/page.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import React, { useEffect, useState } from "react";
+import Loader from "@/components/Loader";
+import Posting from "@/components/form/Posting";
+import { useParams } from "next/navigation";
+
+const EditPost: React.FC = () => {
+    const { id } = useParams();
+
+    const [loading, setLoading] = useState<boolean>(true);
+    const [postData, setPostData] = useState<any>({});
+
+    const getPost = async () => {
+        try {
+            const response = await fetch(`/api/post/${id}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+            const data = await response.json();
+            setPostData(data);
+            setLoading(false);
+        } catch (error) {
+            console.error("Error fetching post:", error);
+        }
+    };
+
+    useEffect(() => {
+        if (id) {
+            getPost();
+        }
+    }, [id]);
+
+    const postInfo = {
+        creatorId: postData?.creator?._id,
+        caption: postData?.caption,
+        tag: postData?.tag,
+        postPhoto: postData?.postPhoto,
+    };
+
+    return loading ? (
+        <Loader />
+    ) : (
+        <div className="pt-6">
+            <Posting post={postInfo} apiEndpoint={`/api/post/${id}`} />
+        </div>
+    );
+};
+
+export default EditPost;
---
+++ src/app/api/post/[id]/route.ts
@@ -0,0 +1,62 @@
+import Post from "@/lib/model/Post.model";
+import { connectDB } from "@/lib/db/mongo";
+import { NextApiRequest } from "next";
+
+export const GET = async (req: NextApiRequest, { params }: any) => {
+    try {
+        await connectDB();
+
+        const post = await Post.findById(params?.id)
+            .populate("creator likes")
+            .exec();
+
+        if (!post) {
+            return new Response("Post not found", { status: 404 })
+        }
+
+        return new Response(JSON.stringify(post), { status: 200 })
+    } catch (err) {
+        console.error(err);
+        return new Response("Failed to get post by id", { status: 404 })
+    }
+};
+
+interface CustomNextApiRequest extends Request {
+    formData(): Promise<any>;
+}
+
+export const POST = async (req: CustomNextApiRequest, { params }: any) => {
+    try {
+        await connectDB();
+
+        const data = await req.formData();
+
+        if (!data || !params?.id) {
+            return new Response("Invalid request data", { status: 400 })
+        }
+
+        const caption = data.get('caption') as string;
+        const tag = data.get('tag') as string;
+
+        const post = await Post.findByIdAndUpdate(
+            params.id,
+            {
+                $set: {
+                    caption: caption,
+                    tag: tag,
+                },
+            },
+            { new: true }
+        );
+
+        if (!post) {
+            return new Response("Post not found", { status: 404 });
+        }
+
+        return new Response(JSON.stringify(post), { status: 200 })
+    } catch (err: any) {
+        console.error(err);
+        return new Response(err.message || "Failed to update post", { status: 500 });
+    }
+};
+
